abe4c00cfe2c89e96da1bcc8cd552007b18ffd24
Moses Martins <mmoses061@gmail.com>
2025-08-26 16:19:50 +0100

    Add logic to upload received image to AWS S3

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,4 +1,25 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+    private readonly s3Client: S3Client;
+    
+    constructor(private readonly configService: ConfigService) {
+        this.s3Client = new S3Client({
+            region: this.configService.getOrThrow('AWS_S3_REGION'),
+        });
+    }
+
+
+    async upload(fileName: string, file: Buffer) {
+        await this.s3Client.send(
+            new PutObjectCommand({
+                Bucket: "outfits-app-bucket",
+                Key: fileName,
+                Body: file,
+            }),
+        );
+    }
+}
