83605c300b2f325c6f99c7c50675e7d3654e0145
Moses Martins <mmoses061@gmail.com>
2025-08-27 11:50:14 +0100

    Update service to return a presigned url and to and objects to get random generated names

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,29 +1,46 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class UploadService {
     private readonly s3Client: S3Client;
-    
+
     constructor(private readonly configService: ConfigService) {
         this.s3Client = new S3Client({
             region: this.configService.getOrThrow('AWS_S3_REGION'),
         });
     }
 
-    async uploadMultiple(files: { fileName: string; file: Buffer }[]) {
-    const uploadPromises = files.map(({ fileName, file }) =>
-        this.s3Client.send(
-            new PutObjectCommand({
-                Bucket: "outfits-app-bucket",
-                Key: fileName,
-                Body: file,
-            }),
-        ),
-    );
+    async uploadMultiple(files: { fileName: string; file: Buffer; fileType: string }[]): Promise<{ randomName: string; signedUrl: string }[]> {
+        const uploadPromises = files.map(async ({ fileName, file, fileType }) => {
 
-    // Wait for all uploads to complete
-    await Promise.all(uploadPromises);
-}
+            const ext = fileName.split('.').pop();
+            const randomName = `${uuid()}.${ext}`;
+
+            await this.s3Client.send(
+                new PutObjectCommand({
+                    Bucket: "outfits-app-bucket",
+                    Key: randomName,
+                    Body: file,
+                    ContentType: fileType,
+                    ContentDisposition: 'inline',
+                }),
+            );
+
+            const command = new GetObjectCommand({
+                Bucket: 'outfits-app-bucket',
+                Key: randomName,
+            });
+            const signedUrl = await getSignedUrl(this.s3Client, command, { expiresIn: 3600 });
+
+            return { randomName, signedUrl }
+
+        });
+
+        // Wait for all uploads to complete
+        return Promise.all(uploadPromises);
+    }
 }
